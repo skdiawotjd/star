98a03352d60904e7d1f52a506fe635f084500152
skdiawotjd <skdiawotjd12@naver.com>
2021-09-17 09:14:05 +0900

    star create

---
+++ Star/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        for (int k = 0; k < 5 - i; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int k = 0; k < 5 - i; k++) {
+            cout << " ";
+        }
+        for (int m = 0; m <= i; m++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    cout << "end";
+    return 0;
+}
